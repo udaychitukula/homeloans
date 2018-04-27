5c62352b091dbe550d9777444a0eb91a2c8d4118
udaychitukula <38734209+udaychitukula@users.noreply.github.com>
2018-04-27 21:58:19 +0530

    new class created

---
+++ src/homeloans/Tests.java
@@ -0,0 +1,11 @@
+package homeloans;
+
+public class Tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test class created");
+	}
+
+}
